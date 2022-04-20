294bd88d53ae5d8e1da3e030daabf627e8c461e5
xiao-jay <2561589453@qq.com>
2022-04-20 11:27:20 +0800

    update webhook.sh

--- webhook.sh
+++ webhook.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-
 cd /root/model-manage
 git pull
+
+screen_name="taskshcedule"
+cmd="go run taskschedule.go -f ./etc/taskschedule-api.yaml"
+main="taskschedule.go"   #进程名
+main_2="taskschedule"
+
+# kill schedule
+echo ps -ef | grep $main | grep -v "grep" | awk -F " " '{print $2}'
+echo ps -ef | grep $main_2 | grep -v "grep" | awk -F " " '{print $2}'
+ps -ef | grep $main | grep -v "grep" | awk -F " " '{print $2}'|xargs kill -9
+ps -ef | grep $main_2 | grep -v "grep" | awk -F " " '{print $2}'|xargs kill -9
+
+# run cmd in screen
+screen -x -S $screen_name -p 0 -X stuff "$cmd"
+screen -x -S $screen_name -p 0 -X stuff $'\n'
